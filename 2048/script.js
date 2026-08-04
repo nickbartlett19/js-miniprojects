a800768ca18a1c5f57a0e65014b5fda86356aaa2
Nick Bartlett <nickbartlett19@gmail.com>
2026-08-03 20:35:40 -0400

    add coloring to tiles

--- 2048/script.js
+++ 2048/script.js
@@ -1,15 +1,37 @@
 const gameBoardTiles = document.querySelectorAll(".game-board-tile");
 
-// console.log(gameBoardTiles);
-
-// console.log(gameBoardTiles[0].children);
-
 gameBoardTiles.forEach(tile => {
 
     if (tile.children[0].innerText === '2') {
-        console.log('change color')
+        tile.classList.add('tile-2');
     }
 
-    // console.log(tile.children[0].innerText);
+    else if (tile.children[0].innerText === '4') {
+        tile.classList.add('tile-4');
+    }
+
+    else if (tile.children[0].innerText === '8') {
+        tile.classList.add('tile-8');
+    }
+
+    else if (tile.children[0].innerText === '16') {
+        tile.classList.add('tile-16');
+    }
+
+    else if (tile.children[0].innerText === '32') {
+        tile.classList.add('tile-32');
+    }
+
+    else if (tile.children[0].innerText === '64') {
+        tile.classList.add('tile-64');
+    }
+
+    else if (tile.children[0].innerText === '128') {
+        tile.classList.add('tile-128-plus');
+    }
+
+    else if (+tile.children[0].innerText > 128) {
+        tile.classList.add('tile-128-plus');
+    }
+
 });
-// console.log('test');
